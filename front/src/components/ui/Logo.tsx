92a1743f709de9734f22156e1d159f1570910ad0
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-06-26 15:58:59 -0300

    Navbar refactor

--- front/src/components/ui/Logo.tsx
+++ front/src/components/ui/Logo.tsx
@@ -3,16 +3,12 @@ import Image from "next/image";
 
 const Logo: React.FC = () => {
   return (
-    <div className="relative flex items-center justify-center mr-9 mb-12">
-      <div className="w-28 h-auto ">
-        <Image src="/LogoHuellas.svg" alt="Logo" className="w-28 h-auto" width={100} height={100} />
+    <div className="flex items-center justify-center">
+      <div className="w-16 h-auto md:w-20 lg:w-28 -mr-6 md:-mr-8 lg:-mr-12 mt-14 ">
+        <Image src="/star_travel.png" alt="Logo" className="w-full h-auto  " width={100} height={100} />
       </div>
-      <div className={`${saira.className}  fixed   flex  items-center justify-center text-white text-center text-lg md:text-2xl lg:text-lg mt-36 mb-4`}>
-        <h1>
-          <span>
-            {/* Huellas de Esperanza */}
-          </span>
-        </h1>
+      <div className={`${saira.className} text-white text-sm md:text-xl lg:text-xl font-semibold`}>
+        <h1 className="whitespace-nowrap">tart Travel</h1>
       </div>
     </div>
   );
