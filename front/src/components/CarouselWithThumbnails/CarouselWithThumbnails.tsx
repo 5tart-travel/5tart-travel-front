ec2330a42cc37632947bfacb1b27118622f3f015
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-06-28 02:57:58 -0300

    imagenes del carrusel reparadas

--- front/src/components/CarouselWithThumbnails/CarouselWithThumbnails.tsx
+++ front/src/components/CarouselWithThumbnails/CarouselWithThumbnails.tsx
@@ -1,137 +1,8 @@
-// import Image from 'next/image';
-// import React from 'react';
-// import Slider from "react-slick";
-// import Card from '@/components/Home_ui/Card_home'; 
-
-// const CarouselWithThumbnails = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     autoplay: true,
-//     autoplaySpeed: 3000, 
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2
-//         }
-//       },
-//       {
-//         breakpoint: 640,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
-//         }
-//       }
-//     ]
-//   };
-
-//   return (
-//     <div className="relative w-full h-[600px] lg:h-[500px] md:h-[400px] sm:h-[300px]">
-//       {/* Banner Image */}
-//       <div className="flex w-full h-[300px] lg:h-[500px] md:h-[400px] sm:h-[300px]">
-//         <Image
-//           src="https://res.cloudinary.com/dia2gautk/image/upload/v1719355788/xuexupstyrirhdwb05bl.jpg"
-//           alt="logo"
-//           className="object-cover w-full h-full"
-//           layout="responsive"
-//           width={1900}
-//           height={600}
-//         />
-//       </div>
-//       {/* Cards Carousel */}
-//       <div className="absolute w-full top-[250px] lg:top-[450px] md:top-[350px] sm:top-[250px] flex justify-center">
-//         <div className="w-[100%]">
-//           <Slider {...settings}>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 1"
-//                 title="Card 1"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 2"
-//                 title="Card 2"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 3"
-//                 title="Card 3"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 4"
-//                 title="Card 4"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 5"
-//                 title="Card 5"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 6"
-//                 title="Card 6"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 7"
-//                 title="Card 7"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 8"
-//                 title="Card 8"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//             <div className="px-4">
-//               <Card
-//                 src="https://via.placeholder.com/100x150"
-//                 alt="Card 9"
-//                 title="Card 9"
-//                 description="Info de la agencia o paquete de viaje xD"
-//               />
-//             </div>
-//           </Slider>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CarouselWithThumbnails;
-
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import Card from '@/components/Home_ui/Card_home';
+import Spiner from '@/components/ui/Spiner';
 
 const CarouselWithThumbnails: React.FC = () => {
   const [tours, setTours] = useState<any[]>([]);
@@ -198,7 +69,7 @@ const CarouselWithThumbnails: React.FC = () => {
       <div className="absolute w-full top-[250px] lg:top-[450px] md:top-[350px] sm:top-[250px] flex justify-center">
         <div className="w-[100%]">
           {loading ? (
-            <p>Loading...</p>
+            <div> <Spiner /> </div>
           ) : tours.length === 0 ? (
             <p>No tours available</p>
           ) : (
@@ -223,4 +94,3 @@ const CarouselWithThumbnails: React.FC = () => {
 };
 
 export default CarouselWithThumbnails;
-
--- front/src/components/Home_ui/Card_home.tsx
+++ front/src/components/Home_ui/Card_home.tsx
@@ -1,36 +1,3 @@
-// import Image from 'next/image';
-// import React from 'react';
-
-// interface CardProps {
-//   src: string;
-//   alt: string;
-//   title: string;
-//   description: string;
-// }
-
-// const Card: React.FC<CardProps> = ({ src, alt, title, description }) => {
-//   return (
-//     <div className="flex flex-col sm:flex-row w-full sm:w-[400px] h-auto sm:h-[150px] bg-white shadow-lg rounded-lg overflow-hidden">
-//       <div className="w-full sm:w-[200px] h-[200px] sm:h-full">
-//         <Image
-//           src={src}
-//           alt={alt}
-//           className="object-cover w-full h-full"
-//           width={200}
-//           height={150}
-//           layout="responsive"
-//         />
-//       </div>
-//       <div className="p-4 flex flex-col justify-center">
-//         <h3 className="text-lg font-bold">{title}</h3>
-//         <p className="text-sm">{description}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Card;
-
 import Image from 'next/image';
 import React from 'react';
 
@@ -45,21 +12,17 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ src, alt, title, price, agency }) => {
   return (
     <div className="flex w-full sm:w-[400px] h-auto sm:h-[150px] bg-white shadow-lg rounded-lg overflow-hidden">
-      <div className="w-[40%]">
+      <div className="w-full sm:w-[200px] h-[200px] sm:h-full relative">
         <Image
           src={src}
           alt={alt}
-          className="object-cover w-full h-full"
-          width={200}
-          height={150}
-          layout="responsive"
+          className="object-cover"
+          layout="fill"
         />
       </div>
-      <div className="w-[60%] p-4 flex flex-col justify-center">
-        <p className="text-xs text-gray-500 uppercase">Ida y Vuelta</p>
-        <h3 className="text-xl font-bold text-blue-600">{title}</h3>
-        <p className="text-sm mt-2">Desde</p>
-        <p className="text-lg font-semibold text-gray-900">${price.toLocaleString()}</p>
+      <div className="p-4 flex flex-col justify-center">
+        <h3 className="text-lg text-blue-800  text-shadow-medium font-bold">{title}</h3>
+        <p className="text-lg font-semibold text-gray-600 ">Precio: ${price.toLocaleString()}</p>
         <p className="text-sm text-gray-500">Agencia: {agency}</p>
       </div>
     </div>
@@ -67,4 +30,3 @@ const Card: React.FC<CardProps> = ({ src, alt, title, price, agency }) => {
 };
 
 export default Card;
-
