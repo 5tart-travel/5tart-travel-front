a0ea75fd80f46e10e1f32ff48a4dddb854a7bae9
MDiaz8778 <maurodiaz8778@gmail.com>
2024-06-29 04:47:25 -0300

    se modifico la vista de ofertas, la de viaje de aviones

--- front/src/app/ofertas/page.tsx
+++ front/src/app/ofertas/page.tsx
@@ -4,6 +4,7 @@ import { IBusTour } from '@/interface/IBusTour';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import TourCard from './tourCard';
+import { da } from '@faker-js/faker';
 
 
 const Ofertas: React.FC = () => {
@@ -41,13 +42,42 @@ const Ofertas: React.FC = () => {
 
   return (
     <main className='bg-gray-50 flex flex-col items-center p-4'>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-6xl">
-        {buses.map((bus) => (
-          <TourCard key={bus.id} tour={bus} />
-        ))}
-      </div>
+      <section className="max-w-6xl w-full mb-8">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center">Paquetes en Oferta</h1>
+        <p className="text-lg text-gray-600 mb-4 text-center">Descubre nuestras mejores ofertas en paquetes turísticos. ¡No te las pierdas!</p>
+      </section>
+      <section className="max-w-6xl w-full mb-8">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-xl font-bold text-gray-800">Paquetes de Avión</h2>
+          <span className="bg-blue-500 text-white px-10 py-1 rounded-full text-sm">Avión</span>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
+          {buses.map((tour) => {
+            if (tour.transportType === 'plane') {
+              return <TourCard key={tour.id} tour={tour} />;
+            }
+            return null;
+          })}
+        </div>
+      </section>
+      <section className="max-w-6xl w-full">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-xl font-bold text-gray-800">Paquetes de Bus</h2>
+          <span className="bg-green-500 text-white px-10 py-1 rounded-full text-sm">Bus</span>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
+          {buses.map((tour) => {
+            if (tour.transportType === 'bus') {
+              return <TourCard key={tour.id} tour={tour} />;
+            }
+            return null; 
+          })}
+        </div>
+      </section>
     </main>
   );
+  
+  
 };
 
 export default Ofertas;
--- front/src/app/travel/pack_bus/page.tsx
+++ front/src/app/travel/pack_bus/page.tsx
@@ -38,47 +38,43 @@ const PackBus: React.FC = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl text-gray-500 font-bold mb-4 text-center">Pack Plane</h1>
+      <h1 className="text-2xl text-gray-500 font-bold mb-4 text-center">Pack Bus</h1>
       {loading && <p>Loading...</p>}
       {error && <p className="text-red-500">{error}</p>}
       <div className="grid grid-cols-8 items-center">
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center ml-7">Origen</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center ml-7">Destino</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center">Agencia</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center">Hotel</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center">Empresa</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center mr-7">Check-in</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center mr-7">Check-out</p>
-        <p className="text-blue-500 mb-4 font-semibold border-b-3 text-center mr-7">Precio</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Origen</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Destino</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Agencia</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Hotel</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Empresa</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Check-in</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Check-out</p>
+        <p className="text-blue-500 mb-4 font-semibold border-b-3">Precio</p>
+       
       </div>
       <div className="space-y-4">
         {buses.map((bus) => (
           <div
             key={bus.id}
-            className={`bg-gray-100 hover:bg-gray-200 shadow-md rounded-lg overflow-hidden p-4 cursor-pointer hover:shadow-lg transition-shadow duration-300 ${
-              bus.oferta ? 'border-2 border-blue-500' : ''
-            }`}
+            className="bg-gray-100 hover:bg-gray-200 shadow-md rounded-lg overflow-hidden p-4 cursor-pointer hover:shadow-lg transition-shadow duration-300"
             onClick={() => handleCardClick(bus.id)}
           >
-            <div className="grid grid-cols-8 items-center text-center">
-              <div className="col-span-1 text-gray-500">{bus.salida}</div>
-              <div className="col-span-1 text-gray-500">{bus.destino}</div>
-              <div className="col-span-1 text-gray-500">{bus.agency.name_agency}</div>
-              <div className="col-span-1 text-gray-500">{bus.hotel}</div>
-              <div className="col-span-1 text-gray-500">{bus.empresa}</div>
-              <div className="col-span-1 text-gray-500">{new Date(bus.fecha_ingreso).toLocaleDateString()}</div>
-              <div className="col-span-1 text-gray-500">{new Date(bus.fecha_egreso).toLocaleDateString()}</div>
-              <div className="col-span-1 text-blue-500 font-bold">
-                {`$${bus.price}`}
-                {bus.oferta && <span className="ml-2 text-blue-500 font-semibold">Oferta</span>}
-              </div>
+            <div className="grid grid-cols-8 items-center">
+              <div className="text-gray-500">{bus.salida}</div>
+              <div className="text-gray-500">{bus.destino}</div>
+              <div className="text-gray-500">{bus.agency.name_agency}</div>
+              <div className="text-gray-500">{bus.hotel}</div>
+              <div className="text-gray-500">{bus.empresa}</div>
+              <div className="text-gray-500">{new Date(bus.fecha_ingreso).toLocaleDateString()}</div>
+              <div className="text-gray-500">{new Date(bus.fecha_egreso).toLocaleDateString()}</div>
+              <div className="text-blue-500 font-bold">{`$${bus.price}`}</div>
+              
             </div>
           </div>
         ))}
       </div>
     </div>
   );
-  
 };
 
 export default PackBus;
