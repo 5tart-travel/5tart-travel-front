62fe3237ba41500196626fef60589849bf74198d
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-06-30 20:09:46 -0400

    Tamaño de imagen arreglado

--- front/src/app/ofertas/tourCard.tsx
+++ front/src/app/ofertas/tourCard.tsx
@@ -1,63 +1,71 @@
 import { FaPlane, FaBus, FaHotel } from 'react-icons/fa';
 import { IBusTour } from '@/interface/IBusTour';
-import Image from 'next/image'; 
-import './tourCard.css'; 
+import Image from 'next/image';
+import './tourCard.css';
 
 interface TourCardProps {
-    tour: IBusTour;
-    onClick: (id: string) => void;
+  tour: IBusTour;
+  onClick: (id: string) => void;
 }
 
 const TourCard: React.FC<TourCardProps> = ({ tour, onClick }) => {
-    return (
-        <div 
-            className="relative bg-white shadow-md rounded-lg overflow-hidden p-4 cursor-pointer hover:shadow-lg transition-shadow duration-300"
-            onClick={() => onClick(tour.id)}  
-        >
-            <div className="flex flex-col relative">
-    <div className="w-full h-48 rounded-lg mb-4 overflow-hidden relative">
-        {tour.oferta && (
-            <div className="ribbon ribbon-top-right text-center"><span>Oferta</span></div>
-        )}
-        <Image
+  return (
+    <div
+      className="relative bg-white shadow-md rounded-lg overflow-hidden p-4 cursor-pointer hover:shadow-lg transition-shadow duration-300"
+      onClick={() => onClick(tour.id)}
+    >
+      <div className="flex flex-col relative">
+        <div className="w-full h-48 rounded-lg mb-4 overflow-hidden relative">
+          {tour.oferta && (
+            <div className="ribbon ribbon-top-right text-center">
+              <span>Oferta</span>
+            </div>
+          )}
+          <Image
             className="w-full h-full absolute inset-0 object-cover"
             src={tour.imgUrl}
             alt={`Imagen de ${tour.destino}`}
-            layout="fill" 
-            objectFit="cover" 
-        />
-    </div>
-</div>
-
-            <div className="grid grid-cols-1 items-center">
-                <div className="flex items-center justify-between mb-2">
-                    <h2 className="text-lg font-bold text-gray-700 truncate">{tour.destino}</h2>
-                    <p className="text-gray-500 font-bold">{tour.agency.name_agency}</p>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="text-gray-500 flex items-center">
-                        {tour.transportType === 'plane' ? <FaPlane className="mr-2 h-5 w-5" /> : <FaBus className="mr-2 h-5 w-5" />}
-                        <p className="text-xs max-w-xs truncate">{tour.empresa}</p>
-                    </div>
-                    <div className="text-gray-500 flex items-center">
-                        <FaHotel className="mr-2 h-5 w-5 self-start" />
-                        <p className="text-xs max-w-xs truncate">{tour.hotel}</p>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4 mt-2">
-                    <p className="bg-white rounded-full border border-gray-300 shadow-md px-4 py-1 text-gray-500 text-xs text-center">
-                        Check-in: {new Date(tour.fecha_ingreso).toLocaleDateString()}
-                    </p>
-                    <p className="bg-white rounded-full border border-gray-300 shadow-md px-4 py-1 text-gray-500 text-xs text-center">
-                        Check-out: {new Date(tour.fecha_egreso).toLocaleDateString()}
-                    </p>
-                </div>
-                <div className="bg-blue-500 text-white font-bold text-center p-2 mt-4 rounded-md shadow-md">
-                    {`$${tour.price}`}
-                </div>
-            </div>
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
-    );
+      </div>
+
+      <div className="grid grid-cols-1 items-center">
+        <div className="flex items-center justify-between mb-2">
+          <h2 className="text-lg font-bold text-gray-700 truncate">
+            {tour.destino}
+          </h2>
+          <p className="text-gray-500 font-bold">{tour.agency.name_agency}</p>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="text-gray-500 flex items-center">
+            {tour.transportType === 'plane' ? (
+              <FaPlane className="mr-2 h-5 w-5" />
+            ) : (
+              <FaBus className="mr-2 h-5 w-5" />
+            )}
+            <p className="text-xs max-w-xs truncate">{tour.empresa}</p>
+          </div>
+          <div className="text-gray-500 flex items-center">
+            <FaHotel className="mr-2 h-5 w-5 self-start" />
+            <p className="text-xs max-w-xs truncate">{tour.hotel}</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4 mt-2">
+          <p className="bg-white rounded-full border border-gray-300 shadow-md px-4 py-1 text-gray-500 text-xs text-center">
+            Check-in: {new Date(tour.fecha_ingreso).toLocaleDateString()}
+          </p>
+          <p className="bg-white rounded-full border border-gray-300 shadow-md px-4 py-1 text-gray-500 text-xs text-center">
+            Check-out: {new Date(tour.fecha_egreso).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="bg-blue-500 text-white font-bold text-center p-2 mt-4 rounded-md shadow-md">
+          {`$${tour.price}`}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TourCard;
