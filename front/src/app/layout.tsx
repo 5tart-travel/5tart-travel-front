08a49307d303280e90996f40d771d7c64bce4843
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-07-05 11:20:57 -0300

    chat add

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -7,6 +7,7 @@ import Navbar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
 import HiddenFooter from "@/components/hidden_footer/HiddenFooter";
 import GloboChat from "@/components/GloboChat/GloboChat";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,7 +30,27 @@ export default function RootLayout({
         <HiddenFooter>
           <Footer username={null} />
         </HiddenFooter>
-        <GloboChat />
+        {/* <GloboChat /> */}
+        <Script id="landbot-script" strategy="lazyOnload">
+          {`
+            window.addEventListener('mouseover', initLandbot, { once: true });
+            window.addEventListener('touchstart', initLandbot, { once: true });
+            var myLandbot;
+            function initLandbot() {
+              if (!myLandbot) {
+                var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+                s.addEventListener('load', function() {
+                  var myLandbot = new Landbot.Livechat({
+                    configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-2540817-1JWJESKA59LWGPZ1/index.json',
+                  });
+                });
+                s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+              }
+            }
+          `}
+        </Script>
       </body>
     </html>
   );
