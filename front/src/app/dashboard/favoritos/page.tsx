9cd25e865aedb8e9a50095841fd997a3ee6210e1
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-07-05 11:39:23 -0400

    ads

--- front/src/app/dashboard/favoritos/page.tsx
+++ front/src/app/dashboard/favoritos/page.tsx
@@ -4,7 +4,7 @@ const Favoritos = () => {
   return (
     <div className='  flex flex-col items-center justify-center bg-red-300 rounded-md md:h-[200px]  ' >
         <div className='text-3xl font-mono font-bold ' >
-          Hola 1,2,3 provando  
+          Hola 1,2,3 probando  
         </div>
     </div>
    
