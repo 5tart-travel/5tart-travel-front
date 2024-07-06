259f6b4bf9da408a6bf91b3eeb88debfaa7a5e7b
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-07-05 20:33:58 -0400

    Arreglando el dashboard de agencia

--- front/src/app/dashboard/mi-perfil/page.tsx
+++ front/src/app/dashboard/mi-perfil/page.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const MiPerfil = () => {
   return (
-    <div className='  flex flex-col items-center justify-center bg-red-300 rounded-md md:h-[200px]  ' >
-        <div className='text-3xl font-mono font-bold ' >
-          Datos del usuario  
-        </div>
+    <div className="  flex flex-col items-center justify-center bg-red-300 rounded-md md:h-[200px]  ">
+      <div className="text-3xl font-mono font-bold ">
+        En reparación... Falta aplicar roles...
+      </div>
     </div>
-   
-  )
-}
+  );
+};
 
-export default MiPerfil
+export default MiPerfil;
--- front/src/app/dashboard/mis-tours/page.tsx
+++ front/src/app/dashboard/mis-tours/page.tsx
@@ -49,20 +49,103 @@ const MisTours = () => {
   }, [token, agencyId]);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
-      {tours.map((tour) => (
-        <Link key={tour.id} href={`/travel/pack_plane/${tour.id}`}>
-          <CardGrid
-            title={tour.title}
-            price={tour.price}
-            region={tour.region}
-            imageUrl={tour.imgUrl}
-            oferta={tour.oferta}
-          />
-        </Link>
-      ))}
+    <div className="p-6">
+      {tours.length === 0 ? (
+        <div className="text-center">
+          <p>
+            Aún no tienes tours, has click{' '}
+            <Link href="/dashboard/agregar-tour" className="text-blue-500">
+              aquí
+            </Link>{' '}
+            para crear uno.
+          </p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {tours.map((tour) => (
+            <Link key={tour.id} href={`/travel/pack_plane/${tour.id}`}>
+              <CardGrid
+                title={tour.title}
+                price={tour.price}
+                region={tour.region}
+                imageUrl={tour.imgUrl}
+                oferta={tour.oferta}
+              />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 export default MisTours;
+
+// 'use client';
+
+// import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { decodeJwt } from '@/utils/decodeJwt';
+// import CardGrid from '@/components/Card_grid/CardGrid';
+// import Link from 'next/link';
+// import { IBusTour } from '@/interface/IBusTour';
+
+// const MisTours = () => {
+//   const [tours, setTours] = useState<IBusTour[]>([]);
+//   const router = useRouter();
+//   const [token, setToken] = useState<string | null>(null);
+//   const [agencyId, setAgencyId] = useState<string | null>(null);
+
+//   useEffect(() => {
+//     const userSessionString: any = localStorage.getItem('userSession');
+
+//     if (userSessionString) {
+//       const userSession = JSON.parse(userSessionString);
+//       const ntoken = userSession.token;
+//       setToken(ntoken);
+
+//       if (ntoken) {
+//         const decoded: any = decodeJwt(ntoken);
+//         const agencyId = decoded.id;
+//         setAgencyId(agencyId);
+//       }
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     if (token && agencyId) {
+//       fetch(`${process.env.NEXT_PUBLIC_API_URL}/agency/${agencyId}`, {
+//         method: 'GET',
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//           'Content-Type': 'application/json',
+//         },
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           setTours(data.tours);
+//         })
+//         .catch((error) => {
+//           console.error('Error al obtener los tours:', error);
+//         });
+//     }
+//   }, [token, agencyId]);
+
+//   return (
+//     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+//       {tours.map((tour) => (
+//         <Link key={tour.id} href={`/travel/pack_plane/${tour.id}`}>
+//           <CardGrid
+//             title={tour.title}
+//             price={tour.price}
+//             region={tour.region}
+//             imageUrl={tour.imgUrl}
+//             oferta={tour.oferta}
+//           />
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default MisTours;
