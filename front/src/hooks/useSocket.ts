a04880238907d059cc862006cd3d42ed702189c4
juank132 <juancarlos_castillo@hotmail.com.ar>
2024-07-11 02:13:41 -0300

    corregido

--- front/src/hooks/useSocket.ts
+++ front/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 // utils/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // Asegúrate de usar la URL correcta de tu servidor
+const socket = io('https://fivetart-travel-kafg.onrender.com'); // Asegúrate de usar la URL correcta de tu servidor
 
 export default socket;
