9314bf52cece163897ad8ab11cf7ba95f76879c5
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-07-13 23:46:07 -0300

    Home responsive 100%

--- front/src/components/Banner_publicidad/B_publicidad.tsx
+++ front/src/components/Banner_publicidad/B_publicidad.tsx
@@ -4,38 +4,35 @@ import React from 'react';
 const TravelBanner: React.FC = () => {
   return (
     <div
-      className="relative   w-full min-h-screen flex flex-col justify-center items-center text-center text-gray-700"
+      className="relative w-full min-h-screen flex flex-col justify-center items-center text-center text-gray-700 bg-no-repeat bg-center bg-contain"
       style={{
         backgroundImage: "url('https://res.cloudinary.com/dia2gautk/image/upload/v1719538399/qlk0ggedcdxekt6jvs1j.webp')",
-        backgroundSize: 'cover',
-        backgroundPosition:  ' center ',
-        backgroundRepeat: 'no-repeat',
-        paddingTop: ' 50px',
-          minHeight: 'calc(100vh - -90px)',
-          maxWidth: '100vw',
+        paddingTop: '50px',
+        minHeight: 'calc(100vh - 90px)',
+        maxWidth: '100vw',
       }}
     >
       <div className="bg-transparent absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center px-4">
-        <h1 className="text-4xl text-blue-950 font-bold text-shadow-semidark sm:text-3xl md:text-4xl">Viaja</h1>
-        <h2 className="text-3xl text-orange-300 font-bold text-shadow-semidark sm:text-2xl md:text-3xl">al mejor destino</h2>
-        <h3 className="text-4xl text-blue-950 font-bold text-shadow-semidark sm:text-3xl md:text-4xl">del mundo</h3>
-        <p className="mt-4 text-base font-normal text-shadow-semilight sm:text-sm md:text-base">
+        <h1 className="text-4xl text-blue-950 font-bold text-shadow-semidark xxs:text-3xl xs:text-4xl sm:text-3xl md:text-4xl">Viaja</h1>
+        <h2 className="text-3xl text-orange-300 font-bold text-shadow-semidark xxs:text-2xl xs:text-3xl sm:text-2xl md:text-3xl">al mejor destino</h2>
+        <h3 className="text-4xl text-blue-950 font-bold text-shadow-semidark xxs:text-3xl xs:text-4xl sm:text-3xl md:text-4xl">del mundo</h3>
+        <p className="mt-4 text-base font-normal text-shadow-semilight xxs:text-sm xs:text-base sm:text-sm md:text-base">
           Descubre paisajes asombrosos <br /> y vive experiencias inolvidables.
         </p>
 
         <div className="mt-10">
           <Link href="/ofertas">
-            <button className="bg-blue-950 hover:bg-blue-800 text-white font-bold py-4 px-4 rounded-2xl shadow-lg sm:py-2 sm:px-3 md:py-3 md:px-4">
+            <button className="bg-blue-950 hover:bg-blue-800 text-white font-bold py-4 px-4 rounded-2xl shadow-lg xxs:py-2 xxs:px-3 xs:py-3 xs:px-4 sm:py-2 sm:px-3 md:py-3 md:px-4">
               Explora ofertas
             </button>
           </Link>
         </div>
 
-        <p className="mt-4 w-11/12 text-2xl font-semibold text-shadow-semilight sm:text-xl md:text-2xl">
+        <p className="mt-4 w-11/12 text-2xl font-semibold text-shadow-semilight xxs:text-xl xs:text-2xl sm:text-xl md:text-2xl">
           ¡Reserva ahora y obtén un <br /> 20% de descuento!
         </p>
 
-        <div className="mt-10 text-sm text-shadow-medium sm:text-xs md:text-sm">
+        <div className="mt-10 text-sm text-shadow-medium xxs:text-xs xs:text-sm sm:text-xs md:text-sm">
           <p>
             Visítanos en <a href="#" className="underline">www.starttravel.com</a> o llámanos al <a href="tel:123-456-7890" className="underline">123-456-7890</a>
           </p>
--- front/src/components/Home_ui/ViajesNacionales.tsx
+++ front/src/components/Home_ui/ViajesNacionales.tsx
@@ -1,27 +1,33 @@
 import Image from 'next/image';
 import React from 'react';
 
-const ViajesNacionales = () => {
+const ViajesNacionales: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center mt-20 md:mt-40 bg-gray-100 h-auto md:h-40">
-      <Image 
-        src="https://res.cloudinary.com/dia2gautk/image/upload/v1719878618/uqjfb5kuwhonq9a44kqr.webp" 
-        alt="Icono de Viajes" 
-        className="mr-0 md:mr-4 mb-4 md:mb-0"
-        width={450}
-        height={300}
-      />
-      <div className="relative flex items-center">
-        <div className="absolute left-0 right-0 h-1 border-dotted border-b border-yellow-500 z-0"></div>
-        <h2 className="relative z-10 px-4 text-2xl md:text-4xl font-bold text-blue-950 text-shadow-semidark text-center md:text-left">Viajes Nacionales</h2>
+    <div className="flex flex-col md:flex-row items-center justify-center mt-20 md:mt-40 bg-gray-100 h-auto  md:h-40 sm:h-40 xs:h-40 xxs:h-32  ">
+      <div className="hidden md:block relative w-full md:w-auto  justify-center items-center">
+        <Image 
+          src="https://res.cloudinary.com/dia2gautk/image/upload/v1719878618/uqjfb5kuwhonq9a44kqr.webp" 
+          alt="Icono de Viajes" 
+          className="mr-0 md:mr-4 mb-4 md:mb-0 object-contain"
+          layout="responsive"
+          width={450}
+          height={300}
+        />
       </div>
-      <Image 
-        src="https://res.cloudinary.com/dia2gautk/image/upload/v1719878618/uqjfb5kuwhonq9a44kqr.webp" 
-        alt="Icono de Viajes" 
-        className="ml-0 md:ml-4 mt-4 md:mt-0 "
-        width={450}
-        height={300}
-      />
+      <div className="relative flex items-center w-full md:w-auto">
+        <div className="absolute left-0 right-0 h-1 border-dotted border-b border-yellow-500 z-0"></div>
+        <h2 className="relative z-10 px-4 text-2xl md:text-4xl font-bold text-blue-950 text-shadow-semidark text-center md:text-left sm:text-4xl xs:text-5xl xxs:text-5xl ">Viajes Nacionales</h2>
+      </div>
+      <div className="hidden md:block relative w-full md:w-auto  justify-center items-center">
+        <Image 
+          src="https://res.cloudinary.com/dia2gautk/image/upload/v1719878618/uqjfb5kuwhonq9a44kqr.webp" 
+          alt="Icono de Viajes" 
+          className="ml-0 md:ml-4 mt-4 md:mt-0 object-contain"
+          layout="responsive"
+          width={450}
+          height={300}
+        />
+      </div>
     </div>
   );
 };
