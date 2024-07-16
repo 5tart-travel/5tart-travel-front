ef4ca82f5ba5c013f69414afe82d40879540c273
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-07-16 13:12:54 -0300

    useParams

--- front/src/app/admin/packages/SearchPackages.tsx
+++ front/src/app/admin/packages/SearchPackages.tsx
@@ -1,41 +1,41 @@
-'use client';
+// 'use client';
 
-import React, { useState, Suspense } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+// import React, { useState, Suspense } from 'react';
+// import { useRouter, useSearchParams } from 'next/navigation';
 
-const SearchBarComponent: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const router = useRouter();
-  const searchParams = useSearchParams();
+// const SearchBarComponent: React.FC = () => {
+//   const [query, setQuery] = useState('');
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newQuery = event.target.value;
-    setQuery(newQuery);
+//   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const newQuery = event.target.value;
+//     setQuery(newQuery);
 
-    const params = new URLSearchParams(searchParams as any); 
-    if (newQuery) {
-      params.set('search', newQuery);
-    } else {
-      params.delete('search');
-    }
-    router.push(`?${params.toString()}`);
-  };
+//     const params = new URLSearchParams(searchParams as any); 
+//     if (newQuery) {
+//       params.set('search', newQuery);
+//     } else {
+//       params.delete('search');
+//     }
+//     router.push(`?${params.toString()}`);
+//   };
 
-  return (
-    <input
-      type="text"
-      value={query}
-      onChange={handleSearch}
-      placeholder="Buscar paquetes..."
-      className="p-2 my-4 rounded-2xl border-none shadow-black/30 shadow-lg"
-    />
-  );
-};
+//   return (
+//     <input
+//       type="text"
+//       value={query}
+//       onChange={handleSearch}
+//       placeholder="Buscar paquetes..."
+//       className="p-2 my-4 rounded-2xl border-none shadow-black/30 shadow-lg"
+//     />
+//   );
+// };
 
-const SearchBar: React.FC = () => (
-  <Suspense fallback={<div>Cargando...</div>}>
-    <SearchBarComponent />
-  </Suspense>
-);
+// const SearchBar: React.FC = () => (
+//   <Suspense fallback={<div>Cargando...</div>}>
+//     <SearchBarComponent />
+//   </Suspense>
+// );
 
-export default SearchBar;
+// export default SearchBar;
