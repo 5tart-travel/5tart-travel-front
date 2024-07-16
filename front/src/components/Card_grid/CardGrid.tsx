c6c0520da74594c716a524dd2745dc727520f422
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-07-16 16:16:47 -0300

    link card home funcional

--- front/src/components/Card_grid/CardGrid.tsx
+++ front/src/components/Card_grid/CardGrid.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React, { FC } from 'react';
 
 export interface CardGridProps {
-  id?: string;
+  id: string;
   title: string;
   price: number;
   location?: string;
@@ -14,6 +14,7 @@ export interface CardGridProps {
   state?: string;
   country?: string;
   empresa?: string;
+  transportType: 'plane' | 'bus';
 }
 
 const CardGrid: FC<CardGridProps> = ({
@@ -28,37 +29,35 @@ const CardGrid: FC<CardGridProps> = ({
   state,
   country,
   empresa,
+  transportType,
 }) => {
   return (
     <div className="w-full h-[300px] rounded-2xl shadow-2xl overflow-hidden relative">
-      <Link href={`/travel/pack_plane/${id}`} key={id}>
-      <Image
-        src={imageUrl}
-        alt={title}
-        className="w-full h-full object-cover transform hover:scale-110 transition duration-300 ease-in-out"
-        width={500}
-        height={500}
-      />
-      {oferta && (
-        <div className="absolute top-0 left-0 bg-blue-950 text-white font-bold py-2 px-4 rounded-br-full">
-          Oferta
+      <Link href={`/travel/pack_${transportType}/${id}`} key={id}>
+        <div>
+          <Image
+            src={imageUrl}
+            alt={title}
+            className="w-full h-full object-cover transform hover:scale-110 transition duration-300 ease-in-out"
+            width={500}
+            height={500}
+          />
+          {oferta && (
+            <div className="absolute top-0 left-0 bg-blue-950 text-white font-bold py-2 px-4 rounded-br-full">
+              Oferta
+            </div>
+          )}
+          <div className="absolute bottom-0 left-0 p-2 bg-blue-600 bg-opacity-35 backdrop-blur-lg shadow-3xl w-[90%] h-auto rounded-2xl flex flex-col mx-4 mb-2">
+            <h3 className="text-xl text-gray-50 text-shadow-semidark font-semibold">
+              {title}
+            </h3>
+            <p className="text-2xl text-gray-50 text-shadow-semidark font-bold">
+              ${price}
+            </p>
+            <p className="text-gray-50">{empresa}</p>
+          </div>
         </div>
-      )}
-
-      <div className=" absolute bottom-0 left-0 p-2 bg-blue-600 bg-opacity-35 backdrop-blur-lg shadow-3xl w-[90%] h-auto rounded-2xl flex flex-col mx-4 mb-2 ">
-        <h3 className="text-xl text-gray-50 text-shadow-semidark font-semibold">{title}</h3>
-        <p className="text-2xl text-gray-50 text-shadow-semidark font-bold ">${price}</p>
-        {/* <p className="text-gray-600"> {location}</p> */}
-        {/* <p className="text-gray-600">{region}</p> */}
-        {/* <p className="text-gray-500"> {hotel}</p> */}
-        {/* <p className="text-gray-500">{state}</p> */}
-        {/* <p className="text-gray-500">{country}</p> */}
-        <p className="text-gray-50">{empresa}</p>
-      </div>
-      
       </Link>
-
-      
     </div>
   );
 };
--- front/src/components/Tour_home/TourHome.tsx
+++ front/src/components/Tour_home/TourHome.tsx
@@ -1,14 +1,14 @@
-// pages/index.tsx
 import React, { useEffect, useState } from 'react';
 import CardGrid from '@/components/Card_grid/CardGrid';
 
 interface Tour {
-  id: number;
+  id: string;
   title: string;
   price: number;
   region: string;
   imgUrl: string;
   oferta: boolean;
+  transportType: 'plane' | 'bus';
 }
 
 const TourHome: React.FC = () => {
@@ -34,15 +34,17 @@ const TourHome: React.FC = () => {
   }, []);
 
   return (
-    <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
       {tours.map((tour) => (
         <CardGrid
           key={tour.id}
+          id={tour.id}
           title={tour.title}
           price={tour.price}
           region={tour.region}
           imageUrl={tour.imgUrl}
           oferta={tour.oferta}
+          transportType={tour.transportType} 
         />
       ))}
     </div>
