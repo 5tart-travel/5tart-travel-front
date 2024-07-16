1ee1e0de57d3d7a8ca445c251007cdd5248ccef3
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-07-16 19:17:19 -0300

    imagenes home

--- front/src/components/Card_grid/CardGrid.tsx
+++ front/src/components/Card_grid/CardGrid.tsx
@@ -14,7 +14,7 @@ export interface CardGridProps {
   state?: string;
   country?: string;
   empresa?: string;
-  transportType: 'plane' | 'bus'; // Nueva prop
+  transportType: 'plane' | 'bus';
 }
 
 const CardGrid: FC<CardGridProps> = ({
@@ -32,15 +32,15 @@ const CardGrid: FC<CardGridProps> = ({
   transportType,
 }) => {
   return (
-    <div className="w-full h-[300px] rounded-2xl shadow-2xl overflow-hidden relative">
+    <div className="relative w-full h-[300px] rounded-2xl shadow-2xl overflow-hidden">
       <Link href={`/travel/pack_${transportType}/${id}`} key={id}>
-        <div>
+        <div className="relative w-full h-full">
           <Image
             src={imageUrl}
             alt={title}
-            className="w-full h-full object-cover transform hover:scale-110 transition duration-300 ease-in-out"
-            width={500}
-            height={500}
+            className="absolute inset-0 w-full h-full object-cover transform hover:scale-110 transition duration-300 ease-in-out"
+            layout="fill"
+            objectFit="cover"
           />
           {oferta && (
             <div className="absolute top-0 left-0 bg-blue-950 text-white font-bold py-2 px-4 rounded-br-full">
@@ -48,8 +48,12 @@ const CardGrid: FC<CardGridProps> = ({
             </div>
           )}
           <div className="absolute bottom-0 left-0 p-2 bg-blue-600 bg-opacity-35 backdrop-blur-lg shadow-3xl w-[90%] h-auto rounded-2xl flex flex-col mx-4 mb-2">
-            <h3 className="text-xl text-gray-50 text-shadow-semidark font-semibold">{title}</h3>
-            <p className="text-2xl text-gray-50 text-shadow-semidark font-bold">${price}</p>
+            <h3 className="text-xl text-gray-50 text-shadow-semidark font-semibold">
+              {title}
+            </h3>
+            <p className="text-2xl text-gray-50 text-shadow-semidark font-bold">
+              ${price}
+            </p>
             <p className="text-gray-50">{empresa}</p>
           </div>
         </div>
