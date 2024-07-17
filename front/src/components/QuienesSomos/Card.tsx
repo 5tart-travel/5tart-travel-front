24d35c39180353f760335e6a7d0298de6f9d81e2
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-07-17 02:55:28 -0300

    .

--- front/src/components/QuienesSomos/Card.tsx
+++ front/src/components/QuienesSomos/Card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from './NuestrosDatos.module.css'
 
 const Card = ( props:any ) => {
@@ -5,18 +6,18 @@ const Card = ( props:any ) => {
 
     return (
         <li className="relative">
-          <div className={`py-2 radius-1 rounded-tr-md rounded-tl-lg ${styles.iconContainer}`}>
+          <div className={`py-2  border-none rounded-tr-2xl rounded-tl-2xl ${styles.iconContainer}`}>
             <h2>{name}</h2>
           </div>
-          <hr className="border-black border-opacity-50 w-full left-0 "></hr>
+          <hr className=" w-full left-0 "></hr>
           <img src={imgUrl} alt="Imagen perfil" className="rounded-full my-2 "></img>
-          <hr className="border-black border-opacity-50 w-full left-0 "></hr>
-          <div className={`flex justify-center items-center gap-2 rounded-br-md rounded-bl-lg py-2 ${styles.iconContainer}`}>
+          <hr className="border-none w-full left-0 "></hr>
+          <div className={`flex justify-center items-center gap-2 rounded-br-2xl rounded-bl-2xl py-2 ${styles.iconContainer}`}>
             <a href={github} target="_blank">
-              <img src="https://res.cloudinary.com/dd1yaduhv/image/upload/v1720991036/github-142-svgrepo-com_ef0qdj.svg" alt="github" className="h-14 hover:opacity-75 bg-white rounded-full"></img>
+              <img src="https://res.cloudinary.com/dd1yaduhv/image/upload/v1720991036/github-142-svgrepo-com_ef0qdj.svg" alt="github" className="h-14 hover:opacity-75  bg-white rounded-full  "></img>
             </a>
             <a href={linkedin} target="_blank">
-              <img src="https://res.cloudinary.com/dd1yaduhv/image/upload/v1720991036/linkedin-round-svgrepo-com_psegzi.svg" alt="linkedin" className="h-custom-3 hover:opacity-75 bg-white rounded-full"></img>
+              <img src="https://res.cloudinary.com/dd1yaduhv/image/upload/v1720991036/linkedin-round-svgrepo-com_psegzi.svg" alt="linkedin" className="h-custom-3 hover:opacity-75 bg-white rounded-full  "></img>
             </a>
           </div>
         </li>
